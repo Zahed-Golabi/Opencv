c59e6ca5788857a430e6dc534cb2a3a9429e963b
Zahed-Golabi <golabizahed@gmail.com>
2023-04-22 14:34:20 +0430

    Opencv_002 added

---
+++ Opencv_002/second.py
@@ -0,0 +1,9 @@
+import cv2
+
+image = cv2.imread("./logo.png")
+# cv2.namedWindow("Input", cv2.WINDOW_AUTOSIZE)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("gray.png", gray)
+cv2.imshow("Gray", gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
